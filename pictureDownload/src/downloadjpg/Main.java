d8426032e08ba6aaecf6b2af95aec7d9f5fd4d3b
sergei-paht <77566158+sergei-paht@users.noreply.github.com>
2021-12-19 19:14:42 +0300

    Скачивание картинки

---
+++ pictureDownload/src/downloadjpg/Main.java
@@ -0,0 +1,64 @@
+package downloadjpg;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static final String IN_FILE_TXT = "D:\\douwlandMusic\\DownPicture\\inFile.txt";
+    private static final String OUT_FILE_TXT = "D:\\douwlandMusic\\DownPicture\\outFile.txt";
+    private static final String PATH_TO_MUSIC = "D:\\douwlandMusic\\DownPicture\\pic";
+
+    public static void main(String[] args) {
+        String Url;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+            while ((Url = inFile.readLine()) != null) {
+                URL url = new URL(Url);
+
+                String result;
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    result = bufferedReader.lines().collect(Collectors.joining("\n"));
+                }
+                Pattern email_pattern = Pattern.compile("\\s*(?<=src\\s?=\\s?\")[^>]*\\/*\\.(jpeg|jpg|png|gif)(?=\")");
+                Matcher matcher = email_pattern.matcher(result);
+                int i = 0;
+                while (matcher.find() && i < 2) {
+                    outFile.write(matcher.group() + "\r\n");
+                    i++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
+            String music;
+            int count = 0;
+            try {
+                while ((music = musicFile.readLine()) != null) {
+                    downloadUsingNIO(music, PATH_TO_MUSIC + String.valueOf(count) + ".jpeg");
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void downloadUsingNIO(String strUrl, String file) throws IOException {
+        URL url = new URL(strUrl);
+        ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+        stream.close();
+        byteChannel.close();
+    }
+}
